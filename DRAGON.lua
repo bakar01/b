bf233b2455df2a53584a7ebfa7ff0840ccc510c9
bakar01 <86725198+bakar01@users.noreply.github.com>
2021-07-22 18:12:52 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -14599,6 +14599,17 @@ end,nil)
 end,nil)
 end
 end
+
+if text == "مسلم" or text == 'سمير عصام' then 
+local function getpro(extra, result, success) 
+if result.photos_[0] then 
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_," سمير المسلم مبرمج السورس @M_S_U ", msg.id_, msg.id_, "md") 
+else 
+send(msg.chat_id_, msg.id_,'سمير المسلم مبرمج السورس @M_S_U', 1, 'md') 
+  end end 
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = 1706080513, offset_ = 0, limit_ = 1 }, getpro, nil) 
+end
+
 if text == 'تغير الايدي' and Manager(msg) then 
 local List = {
 [[
