1001ad7111f420afad439c4ec7f88d4c48073cf2
bakar01 <86725198+bakar01@users.noreply.github.com>
2021-07-22 12:05:48 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -3103,35 +3103,14 @@ local Text = [[
 ┕━━━━━☞[𝑩𝑨𝑲𝑨𝑹](t.me/UU_SD1)☜━━━━━┙
 ]] 
 keyboard = {}  
-keyboard.inline_keyboard = {
-{
-{text = 'مطورين السورس', callback_data="/mdkxkjxjdjxe"},{text = 'المبرمج', callback_data="/dadada"},
-},
-{
-{text = '˹𝑪𝑯˼ ', url="t.me/UU_SD1"},
-},
-}
+keyboard.inline_keyboard = { 
+{{text = '˹B A T O T˼', url="t.me/ba_to_t"},{text = '˹H A M O˼', url="t.me/HaMoO201"}},
+{{text = '˹M U S L I M ˼', url="t.me/M_S_U"}}, 
+{{text = '˹𝑪𝑯 ˼', url="t.me/UU_SD1"}},
+{{text = 'اضف البوت الي مجموعتك ' ,url="t.me/"..dofile("./Bkar.lua").botUserName.."?startgroup=start"}},
+} 
 local msg_id = msg.id_/2097152/0.5 
 https.request("https://api.telegram.org/bot"..token..'/sendvideo?chat_id=' .. msg.chat_id_ .. '&video=https://t.me/M_S_U1/13&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-end
-
-if text == 'سورسي' or text == 'سويس' or text == 'احماحم' or text == 'source' then 
-local Text = [[
-┍━━━━━☞[𝑩𝑨𝑲𝑨𝑹](t.me/UU_SD1)☜━━━━━┑
-    𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶 𝑩𝑨𝑲𝑨𝑹 
-┕━━━━━☞[𝑩𝑨𝑲𝑨𝑹](t.me/UU_SD1)☜━━━━━┙
-]]
-keyboard = {} 
-keyboard.inline_keyboard = {
-{
-{text = 'مطورين السورس', callback_data="/mdkxkjxjdjxe"},{text = 'المبرمج', callback_data="/dadada"},
-},
-{
-{text = '˹𝑪𝑯˼ ', url="t.me/UU_SD1"},
-},
-}
-local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
 if text == 'قناه الاستوري' or text == 'استوري المسلم' or text == 'قناه الاستوريهات' or text == 'الاستوريهات' then 
@@ -17160,28 +17139,6 @@ keyboard.inline_keyboard = {
 return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 
-if Text == '/fafafa' then
-local Teext =[[
-┍┍━━━━━☞[𝑩𝑨𝑲𝑨𝑹](t.me/UU_SD1)☜━━━━━┑
-    𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶 𝑩𝑨𝑲𝑨𝑹 
-┕━━━━━☞[𝑩𝑨𝑲𝑨𝑹](t.me/UU_SD1)☜━━━━━┙
-]] 
-keyboard = {}  
-keyboard.inline_keyboard = {
-{
-{text = 'مطورين السورس', callback_data="/mdkxkjxjdjxe"},{text = 'المبرمج', callback_data="/dadada"},
-},
-{
-{text = '˹𝑪𝑯˼ ', url="t.me/UU_SD1"},
-},
-{
-{text = 'اضف البوت الي مجموعتك ' ,url="t.me/"..dofile("./Bkar.lua").botUserName.."?startgroup=start"},
-},
-}
-local msg_id = msg.id_/2097152/0.5 
-https.request("https://api.telegram.org/bot"..token..'/sendvideo?chat_id=' .. msg.chat_id_ .. '&video=https://t.me/M_S_U1/13&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-end
-
 if Text == '/mdkxkjxjdjxe' then
 local Teext =[[
 ┍━━━━━☞[𝑩𝑨𝑲𝑨𝑹](t.me/UU_SD1)☜━━━━━┑
@@ -17193,7 +17150,7 @@ keyboard.inline_keyboard = {
 {{text = '˹B A T O T˼', url="t.me/ba_to_t"},{text = '˹H A M O˼', url="t.me/HaMoO201"}},
 {{text = '˹𝑪𝑯 ˼', url="t.me/UU_SD1"}},
 {{text = 'اضف البوت الي مجموعتك ' ,url="t.me/"..dofile("./Bkar.lua").botUserName.."?startgroup=start"}},
-{{text = '◗القائمه الرائسيه◖', callback_data="/fafafa"}},
+{{text = '◗القائمه الرائسيه◖', callback_data="/add"}},
 }
 return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
@@ -17209,7 +17166,7 @@ keyboard.inline_keyboard = {
 {{text = '˹M U S L I M ˼', url="t.me/M_S_U"}}, 
 {{text = '˹𝑪𝑯 ˼', url="t.me/UU_SD1"}},
 {{text = 'اضف البوت الي مجموعتك ' ,url="t.me/"..dofile("./Bkar.lua").botUserName.."?startgroup=start"}},
-{{text = '◗القائمه الرائسيه◖', callback_data="/fafafa"}},
+{{text = '◗القائمه الرائسيه◖', callback_data="/add"}},
 }
 return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
